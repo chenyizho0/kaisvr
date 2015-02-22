8dad197c4fadbae1f8347861f4e3e7f108a905f2
chenyizho0 <laixukai@126.com>
2015-02-23 00:56:54 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: laixukai
+    > Mail: laixukai@126.com 
+    > Created Time: 2015年02月23日 星期一 00时54分24秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+	return 0;
+}
